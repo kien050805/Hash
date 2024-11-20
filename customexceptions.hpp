b859fa2e9fc158433bdb3abc32e2b4bc25a38a9d
Bui <bui_h2@red1.cs.denison.edu>
2024-11-19 20:53:12 -0500

    Add RBTree implementation

---
+++ customexceptions.hpp
@@ -0,0 +1,35 @@
+#include <exception>
+#include <iostream>
+
+using namespace std;
+
+class value_not_in_tree : public exception {
+private:
+    const char* message;
+public:
+    // Constructor
+    value_not_in_tree(const char* msg) {
+        message = msg;
+    }
+
+    // Override the what() method
+    const char* what() const noexcept {
+        return message;
+    }
+};
+
+
+class empty_tree_exception : public exception {
+private:
+    const char* message;
+public:
+    // Constructor
+    empty_tree_exception(const char* msg) {
+        message = msg;
+    }
+
+    // Override the what() method
+    const char* what() const noexcept {
+        return message;
+    }
+};
