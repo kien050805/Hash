5ada1bda8f98644cc3bc3002f04492ab4a9e6777
trinitrono <meckel_t1@denison.edu>
2024-11-19 21:00:51 -0500

    Update HashMapTree.hpp

--- HashMapTree.hpp
+++ HashMapTree.hpp
@@ -11,39 +11,19 @@
 
 #include <vector>
 #include <iostream>
+#include "RBTree.hpp"
 
 using namespace std;
 
 template <typename K, typename V>
 class HashMapTree {
 private:
-    struct TreeNode {
-        K key;
-        V value;
-        TreeNode* left;
-        TreeNode* right;
-        int height;
-
-        TreeNode(const K& k, const V& v)
-            : key(k), value(v), left(nullptr), right(nullptr), height(1) {}
-    };
-
-    TreeNode** table; 
+   
+    const long DEFAULT_SLOTS = 1024;
     long slots;
     long size;
-    const long DEFAULT_SLOTS = 1024;
     Hash<K> h;
-
-    // Private helper functions for Complete binary tree operations
-    TreeNode* insertTree(TreeNode* node, const K& key, const V& value);
-    TreeNode* removeTree(TreeNode* node, const K& key);
-    TreeNode* searchTree(TreeNode* node, const K& key);
-    TreeNode* minValueNode(TreeNode* node);
-    int getHeight(TreeNode* node);
-    int getBalance(TreeNode* node);
-    TreeNode* rotateRight(TreeNode* y);
-    TreeNode* rotateLeft(TreeNode* x);
-    void destroyTree(TreeNode* node);
+    vector<RBTree<pair<K, V>>> table;
 
 public:
     HashMapTree();
