57d783c800b14cdb62c6f23040960459f98a29e9
kien050805 <143416413+kien050805@users.noreply.github.com>
2024-11-23 16:13:04 -0500

    HashMap done

--- HashMap.cpp
+++ HashMap.cpp
@@ -11,42 +11,87 @@
 #include "HashMap.hpp"
 #include "customexceptions.hpp"
 
+/**
+ * @brief Construct a new Hash Map<K,V>:: Hash Map object
+ *
+ * @tparam K: templated key data type
+ * @tparam V: templated value data type
+ */
 template <class K, class V>
 HashMap<K, V>::HashMap()
 {
     size = 0;
     slots = DEFAULT_SLOTS;
+    h = Hash<K>(slots, slots);
+
     table = new Node *[slots];
     for (int i = 0; i < slots; i++)
     {
         table[i] = nullptr;
     };
-    h = Hash<K>(slots);
 };
 
+/**
+ * @brief Construct a new Hash Map<K,V>:: Hash Map object
+ *
+ * @tparam K: templated key data type
+ * @tparam V: templated value data type
+ * @param m: number of hash table slots
+ */
 template <class K, class V>
 HashMap<K, V>::HashMap(long m)
 {
+    if (m < 0)
+    {
+        throw size_exception();
+    };
+
     size = 0;
     slots = m;
+    h = Hash<K>(slots, slots);
+
     table = new Node *[slots];
     for (int i = 0; i < slots; i++)
     {
         table[i] = nullptr;
     };
-    h = Hash<K>(slots);
 };
 
+/**
+ * @brief Destroy the Hash Map<K,V>:: Hash Map object
+ *
+ * @tparam K: templated key data type
+ * @tparam V: templated value data type
+ */
 template <class K, class V>
 HashMap<K, V>::~HashMap()
 {
+    for (int i = 0; i < slots; i++)
+    {
+        Node *ptr = table[i];
+        while (ptr != nullptr)
+        {
+            Node *temp = ptr;
+            ptr = ptr->next;
+            delete temp;
+        }
+    }
     delete[] table;
-};
+}
 
+/**
+ * @brief Insert the item <K, V> into the hash table
+ * 
+ * @tparam K: templated key data type
+ * @tparam V: templated value data type
+ * @param key: item key
+ * @param value: item value
+ */
 template <class K, class V>
 void HashMap<K, V>::insert(const K &key, const V &value)
 {
     long slot = h(key);
+
     Node *temp = table[slot];
     while (temp != nullptr)
     {
@@ -65,13 +110,19 @@ void HashMap<K, V>::insert(const K &key, const V &value)
     if (table[slot] != nullptr)
     {
         table[slot]->prev = temp;
-    }
-
+    };
     table[slot] = temp;
 
     size++;
 }
 
+/**
+ * @brief Remove the node D from the hash table
+ * 
+ * @tparam K: templated key data type
+ * @tparam V: templated value data type
+ * @param D: Pointer to the node to be deleted
+ */
 template <class K, class V>
 void HashMap<K, V>::remove(Node *D)
 {
@@ -101,6 +152,13 @@ void HashMap<K, V>::remove(Node *D)
     delete D;
 }
 
+/**
+ * @brief Delete the item with key from the hash table
+ * 
+ * @tparam K: templated key data type
+ * @tparam V: templated value data type
+ * @param key: key of the item to be deleted
+ */
 template <class K, class V>
 void HashMap<K, V>::del(const K &key)
 {
@@ -111,13 +169,23 @@ void HashMap<K, V>::del(const K &key)
         if (temp->item.first == key)
         {
             remove(temp);
-            break;
+            size--;
+            return;
         };
         temp = temp->next;
     };
-    size--;
+
+    throw key_exception();
 };
 
+/**
+ * @brief Access the item with key in the hash table
+ * 
+ * @tparam K: templated key data type
+ * @tparam V: templated value data type
+ * @param key: key of the item to access
+ * @return V&: Value of the item with the given key
+ */
 template <class K, class V>
 V &HashMap<K, V>::operator[](const K &key)
 {
@@ -135,6 +203,14 @@ V &HashMap<K, V>::operator[](const K &key)
     throw key_exception();
 };
 
+/**
+ * @brief Search the item with key in the hash table
+ * 
+ * @tparam K: templated key data type
+ * @tparam V: templated value data type
+ * @param key: key of the item to search for
+ * @return pair<K, V>*: pointer to the item pair
+ */
 template <class K, class V>
 pair<K, V> *HashMap<K, V>::search(const K &key)
 {
@@ -148,5 +224,6 @@ pair<K, V> *HashMap<K, V>::search(const K &key)
         };
         temp = temp->next;
     };
+
     return nullptr;
 };
