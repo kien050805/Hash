a58cb6b5f5cb3c6836164afc7a8744537fface71
kien050805 <143416413+kien050805@users.noreply.github.com>
2024-11-23 16:20:08 -0500

    customeexception add

--- customexceptions.hpp
+++ customexceptions.hpp
@@ -3,33 +3,70 @@
 
 using namespace std;
 
-class value_not_in_tree : public exception {
+class value_not_in_tree : public exception
+{
 private:
-    const char* message;
+    const char *message;
+
 public:
-    // Constructor
-    value_not_in_tree(const char* msg) {
+    value_not_in_tree(const char *msg)
+    {
         message = msg;
     }
 
-    // Override the what() method
-    const char* what() const noexcept {
+    const char *what() const noexcept
+    {
         return message;
     }
 };
 
-
-class empty_tree_exception : public exception {
+class empty_tree_exception : public exception
+{
 private:
-    const char* message;
+    const char *message;
+
 public:
-    // Constructor
-    empty_tree_exception(const char* msg) {
+    empty_tree_exception(const char *msg)
+    {
         message = msg;
     }
 
-    // Override the what() method
-    const char* what() const noexcept {
+    const char *what() const noexcept
+    {
+        return message;
+    }
+};
+
+class key_exception : public exception
+{
+private:
+    const char *message;
+
+public:
+    key_exception(const char *msg)
+    {
+        message = msg;
+    }
+
+    const char *what() const noexcept
+    {
+        return message;
+    }
+};
+
+class size_exception : public exception
+{
+private:
+    const char *message;
+
+public:
+    size_exception(const char *msg)
+    {
+        message = msg;
+    }
+
+    const char *what() const noexcept
+    {
         return message;
     }
 };
